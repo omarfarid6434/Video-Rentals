058fc765cf3ab2b82e8746f104e80df5e7a57c28
Frarid Hossain <omarfarid778@gmail.com>
2018-07-18 02:56:21 +0600

    add View Model

---
+++ Video-Rentals/Video-Rentals/ViewModels/RandomMovieViewModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Video_Rentals.Models;
+
+namespace Video_Rentals.ViewModels
+{
+    public class RandomMovieViewModels
+    {
+        public Movie Movie { get; set; }
+        public List<Customers> Customers { get; set; }
+    }
+}
