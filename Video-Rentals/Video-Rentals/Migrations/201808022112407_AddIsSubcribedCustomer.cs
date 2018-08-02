75aa997bf4600eedb715c04e744a9ea5c7044dc5
Frarid Hossain <omarfarid778@gmail.com>
2018-08-03 03:15:15 +0600

    Update Customer Domain Model

---
+++ Video-Rentals/Video-Rentals/Migrations/201808022112407_AddIsSubcribedCustomer.cs
@@ -0,0 +1,18 @@
+namespace Video_Rentals.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddIsSubcribedCustomer : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Customers", "IsSubcribedToNewsLetter", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Customers", "IsSubcribedToNewsLetter");
+        }
+    }
+}
--- Video-Rentals/Video-Rentals/Models/Customer.cs
+++ Video-Rentals/Video-Rentals/Models/Customer.cs
@@ -10,5 +10,6 @@ namespace Video_Rentals.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public bool IsSubcribedToNewsLetter { get; set; }
     }
 }
