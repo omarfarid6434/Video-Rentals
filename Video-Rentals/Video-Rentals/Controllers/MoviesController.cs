3b09b4d67b07c5ce572449fec716c55e4ab906ea
Frarid Hossain <omarfarid778@gmail.com>
2018-09-27 04:53:19 +0600

    Display the list of Movie

--- Video-Rentals/Video-Rentals/Controllers/MoviesController.cs
+++ Video-Rentals/Video-Rentals/Controllers/MoviesController.cs
@@ -3,15 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Video_Rentals.Models;
+using System.Data.Entity;
+
 
 namespace Video_Rentals.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies
-        public ActionResult Index()
+        private ApplicationDbContext _context;
+
+        public MoviesController()
         {
-            return View();
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+
+        // GET: Movies
+
+        public ViewResult Index()
+        {
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
+
+            return View(movies);
+        }
+        public ActionResult Details( int id) {
+
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+
+                return HttpNotFound();
+
+            return View(movie);
         }
     }
 }
--- Video-Rentals/Video-Rentals/Models/Movie.cs
+++ Video-Rentals/Video-Rentals/Models/Movie.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 using Video_Rentals.Models;
 
 namespace Video_Rentals.Models
@@ -9,6 +10,17 @@ namespace Video_Rentals.Models
     public class Movie
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
+        [Required]
+        public Genre Genre { get; set; }
+        public byte GenreId { get; set; }
+
+        public DateTime DateAdded { get; set; }
+        public DateTime ReaslesDate { get; set; }
+        public byte NumeberOfStock { get; set; }
+
+
     }
 }
